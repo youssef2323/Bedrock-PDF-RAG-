796e2c5885a0b0a0cef1e4d71ce72240a4d4730a
youssef2323 <94356894+youssef2323@users.noreply.github.com>
2025-10-19 09:13:48 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,148 @@
+import json
+import os
+import sys
+import boto3
+import streamlit as st
+
+
+## we will use Titan Embeddings Model to generate Embedding
+
+from langchain_community.embeddings import BedrockEmbeddings
+from langchain_community.chat_models import BedrockChat
+
+
+## Data Ingestion
+
+import numpy as np
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import PyPDFDirectoryLoader
+
+
+## Vector Embedding And Vector store
+
+from langchain_community.vectorstores import FAISS
+
+## LLm Models
+from langchain.prompts import PromptTemplate
+from langchain.chains import RetrievalQA
+from langchain.chains import create_retrieval_chain
+
+
+## Bedrock clients
+bedrock = boto3.client(service_name="bedrock-runtime", region_name="us-east-1")
+bedrock_embeddings = BedrockEmbeddings(
+    model_id="amazon.titan-embed-text-v2:0", client=bedrock
+)
+
+## Data ingestion
+
+
+def data_ingestion():
+    loader = PyPDFDirectoryLoader("data")
+    documents = loader.load()
+
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=2000, chunk_overlap=400)
+    docs = text_splitter.split_documents(documents)
+    return docs
+
+
+## Vector Embedding and vectorstore
+def get_vector_store(docs):
+    vectorstore_faiss = FAISS.from_documents(docs, bedrock_embeddings)
+    vectorstore_faiss.save_local("faiss_index")
+
+
+## LLM Model
+
+
+def get_claude_llm():
+    ## get the anthropic model
+    llm = BedrockChat(
+        model_id="anthropic.claude-3-sonnet-20240229-v1:0",
+        client=bedrock,
+        model_kwargs={"max_tokens": 512},
+    )
+
+    return llm
+
+
+def get_mistral_llm():
+    ## get the llama3 model
+    llm = BedrockChat(
+        model_id="mistral.mistral-7b-instruct-v0:2",
+        client=bedrock,
+        model_kwargs={"max_tokens": 512},
+    )
+    return llm
+
+
+prompt_template = """   
+
+Human: Use the following pieces of context to provide a
+concise answer to the question at the end but use atleast summary of 250 words
+with detailed explanations. If you don't know the answer,
+just say that you don't know, don't try to make up an answer.
+<context>
+{context}
+</context>
+
+Question: {question}
+
+Assistant
+
+"""
+
+PROMPT = PromptTemplate(
+    template=prompt_template, input_variables=["context", "question"]
+)
+
+
+def get_response_llm(llm, vectorstore_faiss, question):
+    qa = RetrievalQA.from_chain_type(
+        llm=llm,
+        chain_type="stuff",
+        retriever=vectorstore_faiss.as_retriever(
+            search_type="similarity", search_kwargs={"k": 3}
+        ),
+        return_source_documents=True,
+        chain_type_kwargs={"prompt": PROMPT},
+    )
+
+    answer = qa({"query": question})
+    return answer["result"]
+
+
+## Streamlit app
+def main():
+    st.set_page_config("Chat PDF")
+    st.header("Chat with PDF using AWS Bedrock")
+
+    user_question = st.text_input("Ask a Question from the PDF Files")
+
+    with st.sidebar:
+        st.title("Update Or Create Vector Store:")
+        if st.button("Vectors Update"):
+            with st.spinner("Processing..."):
+                docs = data_ingestion()
+                get_vector_store(docs)
+                st.success("Done")
+    if st.button("Claude Output"):
+        with st.spinner("Processing..."):
+            faiss_index = FAISS.load_local(
+                "faiss_index", bedrock_embeddings, allow_dangerous_deserialization=True
+            )
+            llm = get_claude_llm()
+            st.write(get_response_llm(llm, faiss_index, user_question))
+            st.success("Done")
+    if st.button("mistralai Output"):
+        with st.spinner("Processing..."):
+            faiss_index = FAISS.load_local(
+                "faiss_index", bedrock_embeddings, allow_dangerous_deserialization=True
+            )
+            llm = get_mistral_llm()
+            st.write(get_response_llm(llm, faiss_index, user_question))
+            st.success("Done")
+
+
+if __name__ == "__main__":
+    main()
